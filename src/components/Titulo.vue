6850043577798d77bf75af66c40a6ef5f4531b04
Jose García Máñez <josegarciamanez@gmail.com>
2018-11-23 19:00:12 +0100

    ejercicios jest1

---
+++ src/components/Titulo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ titulo }}</h1>
+        <h5>{{ autor }}</h5>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Titulo',
+    props: {
+        titulo: String,
+        autor: String,
+    },
+    data() {
+        return {};
+    },
+};
+</script>
